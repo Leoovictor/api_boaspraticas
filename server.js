9cfafd57114bb32bfeea035cad517afaeb9ef095
Leoovictor <110347241+Leoovictor@users.noreply.github.com>
2023-07-10 11:55:41 -0300

    mais um

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const http = require("http")
 const port = 3000
 
+
+const rotas ={
+    '/':'Curso Node',
+    '/livros':'livro',
+    '/autores':'Listagem de autores',
+    '/sobre':'asdasdasd'
+}
+
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'})
-    res.end('CURSO NODEEE')
+    res.end(rotas[req.url])
 })
 
-server.listen(port, () => {
-    console.log(`Rodando http://localhost:${port}`)
-})
+
+server.listen(port, () => console.log(`Rodando http://localhost:${port}`))
